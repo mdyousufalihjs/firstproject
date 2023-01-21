bc03ec5f55b30024732caea30b3e4ceff986b70b
Your Name <you@example.com>
2023-01-21 17:29:47 +0600

    firstt

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.slick-slid').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover:false,
+        prevArrow: '.left',
+        nextArrow: '.right',
+      });
+})
